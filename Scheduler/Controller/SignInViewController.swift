deeda3ddf72ead46cf424233140eaa04857fa60c
Gustavo Brunetto <gustavobruneto@gmail.com>
2020-05-06 16:51:12 -0700

    add sign in function

--- Scheduler/Controller/SignInViewController.swift
+++ Scheduler/Controller/SignInViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loadingSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,14 +27,62 @@ class SignInViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signIn(_ sender: Any) {
+        guard let name = nameTextField.text else {
+            displayAlert(message: "Invalid name")
+            return
+        }
+        
+        guard let email = emailTextField.text else {
+            displayAlert(message: "Invalid email")
+            return
+        }
+        
+        guard let password = passwordTextField.text else {
+            displayAlert(message: "Invalid password")
+            return
+        }
+        
+        if email.isEmpty || password.isEmpty {
+            displayAlert(message: "Email/password invalid")
+            return
+        }
+        
+        setupLogin(isLoading: true)
+        
+        SchedulerClient.signIn(name: name, email: email, password: password) { (success, error) in
+            if !success {
+                self.displayAlert(message: error?.localizedDescription ?? "Something went wrong :(")
+                self.setupLogin(isLoading: false)
+                return
+            }
+            
+            self.handleSignInSuccess()
+        }
     }
-    */
-
+    
+    private func handleSignInSuccess() {
+        let mainTabBarController = storyboard?.instantiateViewController(identifier: "MainTabBar") as! MainTabBarViewController
+        navigationController?.pushViewController(mainTabBarController, animated: true)
+    }
+    
+    
+    private func setupLogin(isLoading: Bool) {
+        if isLoading {
+            loadingSpinner.startAnimating()
+        } else {
+            loadingSpinner.stopAnimating()
+        }
+        
+        signInButton.isEnabled = !isLoading
+        nameTextField.isEnabled = !isLoading
+        emailTextField.isEnabled = !isLoading
+        passwordTextField.isEnabled = !isLoading
+    }
+    
+    private func displayAlert(message: String) {
+        let alertView = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertView, animated: true, completion: nil)
+    }
 }
